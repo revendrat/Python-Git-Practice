3dca976169fb9920bf7aad203be7b9e18aab9ffe
Revendra <t.revendra@gmail.com>
2023-05-20 23:07:51 -0400

    Create interest_rate.py

---
+++ interest_rate.py
@@ -0,0 +1,6 @@
+# write a program that calculates future value on a given principal and interest rate
+pv = 1000 #(in dollars)
+r = 0.045 #(interest rate divided over 100)
+n = 5 #(number of years)
+
+fv = pv*pow((1+r),n)
