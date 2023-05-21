53bc08528f87c915121c178b7cc86013da6f39ec
mounika <mounika@gmail.com>
2023-05-21 09:32:02 -0400

    Added Compounding

--- interest_rate.py
+++ interest_rate.py
@@ -5,8 +5,10 @@ n = 5 #(number of years)
 
 #fv = pv*pow((1+r),n)
 
-def inretest_rate(pv,r,n):
-    fv = pv*pow((1+r)),n)
+
+def inretest_rate(pv,r,n,k):
+    
+    fv = pv*pow((1+r/k),n)
     return fv
 
-inretest_rate(1000,0.05,2)
+inretest_rate(1000,0.05,2,2)
