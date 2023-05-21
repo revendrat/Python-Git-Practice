7181938fc40e08a9e7bd377e11ec25c5a12abf97
mounika <mounika@gmail.com>
2023-05-21 09:03:42 -0400

    FunctionAdded
    Commit

--- interest_rate.py
+++ interest_rate.py
@@ -3,4 +3,10 @@ pv = 1000 #(in dollars)
 r = 0.045 #(interest rate divided over 100)
 n = 5 #(number of years)
 
-fv = pv*pow((1+r),n)
+#fv = pv*pow((1+r),n)
+
+def inretest_rate(pv,r,n):
+    fv = pv*pow((1+r)),n)
+    return fv
+
+inretest_rate(1000,0.05,2)
